7027bfbe434f0ad8b002cd7187dfb44e26fd7178
AFRIAULAR <aularafrica@gmail.com>
2025-04-05 21:50:17 -0300

    Update: scene and character controller changes

--- Assets/Scripts/PersonController.cs
+++ Assets/Scripts/PersonController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PersonController : MonoBehaviour
 {
+    [Header("Character Movement")] //Tag para identificar variables en inspector
     public float speed = 5f;
-   public CharacterController characterController;
-    void Start()
-    {
-        
-    }
+    public CharacterController characterController;
 
+    [Header("Camera Movement")]
+    public Transform cameraTransform;
+    public float mouseSensitivityX = 2f;
+    public float mouseSensitivityY = 2f;
+    private float verticalRotation = 0f;
     void Update()
     {
         MovePlayer();
-        
+        MoveCamera();
     }
 
     void MovePlayer()
@@ -27,4 +29,18 @@ public class PersonController : MonoBehaviour
 
        characterController.Move(movement * speed * Time.deltaTime);
     }
+
+    void MoveCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY;
+
+        //CLAMP (rango de alcance de la camara)
+        verticalRotation += mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -45f, 45f);
+
+        cameraTransform.localRotation = Quaternion.Euler(verticalRotation,0,0f); //rotacion vertical para mover la camara, arrastrar camara al script en inspector para que funcione
+        transform.Rotate(Vector3.up * mouseX);
+    }
 }
+ 
